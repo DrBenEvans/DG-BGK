7865d9eae1672958fc586f293e88ab9a7f64a201
Michele Mesiti <michele.mesiti@gmail.com>
2018-01-09 17:48:14 +0000

    Done a first pass of modifications.
    Things that might have gone wrong:
    - Use of the MPI_COMM_WORLD communicator not changed to MPI_COMM_P when
    necessary (fixed a couple of occurrences of this already)
    - DISFN-like arrays not being indexed correctly
    - "RANK" appearing instead of MPI_RANK_P ( or MPI_RANK_V)
    - limits for MPI_RANK_P (MPI_SIZE_P includes the master or not? I think
    so but then MPI_SIZE_P == NGRPS+1).
    
    Moreover, the modifications to EDGFLX should be rationalized.

--- src/OUTPUT.f90
+++ src/OUTPUT.f90
@@ -1,5 +1,6 @@
-      SUBROUTINE OUTPUT(NNODE ,NPOIN_PP ,NPOIN,NELEM ,NELEM_PP,COORD,& 
-     &               INTMA , DISNF_PP, VNPNT,IPCOM_PP,ELGRP,IVD,&
+      SUBROUTINE OUTPUT(NNODE,NPOIN_PP,NPOIN,NELEM,NELEM_PP,&
+     &              maxNELEM_pp,COORD,INTMA , DISNF_PP, VNPNT,IPCOM_PP,&
+     &                     ELGRP,NEGRP,IVD,&
      &                     MPI_RANK_P,MPI_SIZE_P,MPI_COMM_P,&
      &                     MPI_RANK_V,MPI_SIZE_V,MPI_COMM_V,&
      &                     VSPACE_FIRST,VSPACE_LAST) 
@@ -41,19 +42,22 @@
         END TYPE
 ! 
 	  INTEGER VNPNT,NPOIN,NELEM,NNODE,IP,IN,NRANK,NELEM_PP 
+      INTEGER maxNELEM_PP  
       INTEGER IPT,J,IE,MPI_IERR,MPI_STATUS(MPI_STATUS_SIZE) 
       INTEGER IE_PP,TAG 
       INTEGER NPOIN_PP,IPCOM_PP(NPOIN_PP),IV,ELGRP(NELEM,2) 
       REAL COORD(2,NPOIN) 
+      INTEGER NEGRP(MPI_SIZE_P-1)
 ! 
       INTEGER INTMA(NNODE,NELEM) 
       ! mpi-stuff for position space partitioning
       INTEGER MPI_RANK_P,MPI_SIZE_P,MPI_COMM_P,GROUP_P
       ! mpi-stuff for velocity space partitioning
-      INTEGER MPI_RANK_V,MPI_SIZE_V,MPI_COMM_V,GROUP_V
-      INTEGER VNPNT_PART,VSPACE_FIRST,VSPACE_LAST
-      REAL DISNF_PP(NNODE,VSPACE_FIRST:VSPACE_LAST,NELEM_PP)
+      INTEGER MPI_RANK_V,MPI_SIZE_V,MPI_COMM_V,GROUP_V,IVP
+      INTEGER VNPNT_PART,VSPACE_FIRST,VSPACE_LAST,IG,TSIZE
+      REAL DISNF_PP(NNODE,VSPACE_FIRST:VSPACE_LAST,maxNELEM_PP)
       REAL DISNF(NNODE,VSPACE_FIRST:VSPACE_LAST,NELEM)
+      INTEGER VNPNT_PART
 
 
 !
@@ -73,68 +77,80 @@
         OPEN  (20, file=filename, status='UNKNOWN')  
     
         WRITE(20,185)  
-        WRITE(20,*)  NPOIN,'    ',VNPNT 
-      ENDIF !LINKS WITH IF STATEMENT ON LINE 20 
+        WRITE(20,*)  NPOIN,'    ',VNPNT  
+      ENDIF
       CALL MPI_BARRIER(MPI_COMM_WORLD,MPI_IERR) 
-! 
-! *** COLLECT THE LOCAL DISNF_PP's AND WRITE TO OUTPUT FILE 
-! *** LOOP OVER THE V-SPACE NODES AND GLOBAL ELEMENTS 
+    
+      VNPNT_PART = VSPACE_LAST-VSPACE_FIRST+1
+      DO IG=1,MPI_SIZE_P !
+        TSIZE = NEGRP(IG)*3*VNPNT_PART
+        IF(MPI_RANK_P.EQ.IG)THEN
+          MPI_SEND(DISNF_PP,TSIZE,MPI_REAL,0,1,&
+     &            MPI_COMM_P,MPI_IERR)
+        ENDIF
+        IF(MPI_RANK_P.EQ.0)THEN
+          MPI_RECV(DISNF_PP,TSIZE,MPI_REAL,IG,1,&
+     &            MPI_COMM_P,MPI_IERR)
+          ! Copying DISNF_PP into DISNF    
+          DO IE=1,NELEM ! scanning through DISNF
+          NRANK = ELGRP(IE,1) ! Checking if we have received the 
+                              ! necessary data in this iteration of the
+                              ! DO IG,MPI_SIZE_P group
+            IF(NRANK.EQ.IG)THEN
+              IE_PP=ELGRP(IE,2)
+              DISNF(:,:,IE) = DISNF_PP(:,:,IE_PP)
+            ENDIF
+          ENDDO
+        ENDIF
+      ENDDO
+      
+      ! only the master ranks in the MPI_COMM_P communicators
+      IF(MPI_RANK_P.EQ.0)THEN !jdfcdknsdasdjkha
+        TSIZE =  NELEM*3*VNPNT_PART
+        DO IVP=0,MPI_SIZE_V-1 !dvkjdkjdjkshasdjkh
+          IF((IVP.EQ.MPI_RANK_V).AND.(IVP.NE.0)) THEN
+              MPI_SEND(DISNF,TSIZE,MPI_REAL,0,1,&
+     &             MPI_COMM_V,MPI_IERR)
+          ENDIF          
+          IF(0.EQ.MPI_RANK_V) THEN !dsaaanccmna
+            IF(IVP.NE.0) MPI_RECV(DISNF,TSIZE,MPI_REAL,IVP,1,&
+     &             MPI_COMM_V,MPI_IERR)
 
+            DO IV=VSPACE_FIRST,VSPACE_LAST ! 1 to VNPNT_PART for rank 0
+              DO IE=1,NELEM
+                WRITE(20,*) (DISNF(IN,IV,IE),IN=1,NNODE)
+              ENDDO
+            ENDDO
+          ENDIF ! IF(0.EQ.MPI_RANK_V) THEN !dsaaanccmna
+        ENDDO ! DO IVP=0,MPI_SIZE_V-1 !dvkjdkjdjkshasdjkh
 
-!   
-      DO 1000 IV=1,VNPNT 
-        DO 1001 IE=1,NELEM 
-          IF(RANK.EQ.0)THEN 
-            NRANK=ELGRP(IE,1) 
-            IE_PP=ELGRP(IE,2) 
-          ENDIF 
-          CALL MPI_BCAST(NRANK,1,MPI_INTEGER,0,MPI_COMM_WORLD,MPI_IERR) 
-          CALL MPI_BCAST(IE_PP,1,MPI_INTEGER,0,MPI_COMM_WORLD,MPI_IERR) 
-          IF(RANK.EQ.NRANK)THEN 
-            DO IN=1,NNODE 
-              DISNFPARCEL(IN)=DISNF_PP(IN,IV,IE_PP) 
-            ENDDO 
-            TAG=1 
-            CALL MPI_SEND(DISNFPARCEL,NNODE,MPI_REAL,0,TAG,MPI_COMM_WORLD,& 
-     &        MPI_IERR) 
-          ENDIF 
-          IF(RANK.EQ.0)THEN 
-            TAG=1 
-            CALL MPI_RECV(DISNFPARCEL,NNODE,MPI_REAL,NRANK,TAG,& 
-     &        MPI_COMM_WORLD,MPI_STATUS,MPI_IERR) 
-            WRITE(20,*) (DISNFPARCEL(IN),IN=1,NNODE) 
-          ENDIF 
- 1001   CONTINUE 
- 1000 CONTINUE	   
-	  IF(RANK.EQ.0)THEN 
-	  CLOSE(20) 
+        IF(MPI_RANK_V.EQ.0)THEN !lkjfsdccddna
+          CLOSE(20) 
+! ***     OUTPUT FOR GID MESHFILE 
+! ***     SET UP CHANNEL 
+          filename = IVD%GIDMeshFile 
+          PRINT*,'OPENING GIDMESH FILE = ',IVD%GIDMeshFile 
+          OPEN  (19, file=filename, status='unknown') 
+          WRITE(*,*) 
 ! 
-! *** OUTPUT FOR GID MESHFILE 
+          WRITE(19,210)  
+          WRITE(19,300) 
+          DO IP=1,NPOIN 
+            WRITE(19,400)IP,(COORD(J,IP),J=1,2) 
+          ENDDO
+          WRITE(19,220) 
 ! 
-! *** SET UP CHANNEL 
+          WRITE(19,230) 
+          DO IE=1,NELEM 
+            WRITE(19,200)IE,(INTMA(J,IE),J=1,NNODE) 
+          ENDDO
+          WRITE(19,240) 
 ! 
-      filename = IVD%GIDMeshFile 
-      PRINT*,'OPENING GIDMESH FILE = ',IVD%GIDMeshFile 
-      OPEN  (19, file=filename, status='unknown') 
-      WRITE(*,*) 
-! 
-	  WRITE(19,210)  
-      WRITE(19,300) 
-      DO 6000 IP=1,NPOIN 
-      WRITE(19,400)IP,(COORD(J,IP),J=1,2) 
- 6000 CONTINUE 
- 	  WRITE(19,220) 
-! 
-	  WRITE(19,230) 
-      DO 6001 IE=1,NELEM 
-      WRITE(19,200)IE,(INTMA(J,IE),J=1,NNODE) 
- 6001 CONTINUE 
- 	  WRITE(19,240) 
-! 
-	  CLOSE(19) 
-      WRITE(*,*) 
-      WRITE(*,*) 
-      ENDIF !LINKS WITH IF STATEMENT ON LINE 53 
+          CLOSE(19) 
+          WRITE(*,*) 
+          WRITE(*,*) 
+        ENDIF ! IF(MPI_RANK_V.EQ.0)THEN !lkjfsdccddna
+      ENDIF ! IF(MPI_RANK_P.EQ.0)THEN !jdfcdknsdasdjkha
 ! 
 ! *** format statements 
 !	 
