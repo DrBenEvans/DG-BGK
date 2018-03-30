f240269b9871b2df53499b130cbe904b47e1351e
Michele Mesiti <michele.mesiti@gmail.com>
2018-03-30 22:21:23 +0100

    Issue in INICON: IVD structure didn't match the one in spectralBGK
    fixed

--- src/INICON.f90
+++ src/INICON.f90
@@ -10,23 +10,37 @@
        IMPLICIT NONE 
       INCLUDE 'mpif.h' 
       TYPE InputVariables
-          INTEGER :: TORDER !Quadrature order in Theta for V-SPACE
-          INTEGER :: NTIME !Number of timesteps
-          INTEGER :: IMMAT !Lumped mass matrices (no/yes)
-          INTEGER :: RS !Using Restart data (no/yes)
-          INTEGER :: INF !Is there an inflow (no/yes)
-          INTEGER :: NVSPACEPART ! number of VSPACE partitions
-          REAL :: CSAFM !Safety factor applied to timestep (Courant)
-          REAL :: rv !Radial extent of the V-SPACE
-          REAL :: T1 !Initial condition temp
-          REAL :: P1 !Initial condition pressure
-          REAL :: U0 !Initial condition X-vel
-          REAL :: V0 !Initial condition Y-vel
-          REAL :: CINF(4)
-          REAL :: W
-          REAL :: ALPHA
-      END TYPE
-
+        INTEGER :: TORDER !Quadrature order in Theta for V-SPACE
+        INTEGER :: NTIME !Number of timesteps
+        INTEGER :: FORCEOUT !Force output at each timestep (1 = yes, 0 = no)
+        INTEGER :: IMMAT !Lumped mass matrices (no/yes)
+        INTEGER :: RS !Using Restart data (no/yes)
+        INTEGER :: INF !Is there an inflow (no/yes)
+        INTEGER :: NVSPACEPART ! number of VSPACE partitions
+        REAL :: CSAFM !Safety factor applied to timestep (Courant)
+        REAL :: rv !Radial extent of the V-SPACE
+        REAL :: T1 !Initial condition temp
+        REAL :: P1 !Initial condition pressure
+        REAL :: U0 !Initial condition X-vel
+        REAL :: V0 !Initial condition Y-vel
+        REAL :: CINF(4)
+        REAL :: W
+        REAL :: ALPHA !WALL MOLECULAR REFLECTION PARAMETER
+        REAL :: R !GAS CONSTANT
+        REAL :: d !MOLECULAR DIAMETER
+        REAL :: M !MOLAR MASS
+        CHARACTER :: LobattoFile*80
+        CHARACTER :: PSpaceFile*80
+        CHARACTER :: OutFile*80
+        CHARACTER :: PartitionFile*80
+        CHARACTER :: RestartInFile*80
+        CHARACTER :: ResidualFile*80
+        CHARACTER :: ResultsFile1*80
+        CHARACTER :: ResultsFile2*80
+        CHARACTER :: RestartOutFile*80
+        CHARACTER :: GIDMeshFile*80  
+      END TYPE  
+!
 ! *** DECLARE VARIABLES 
 ! 
       TYPE(InputVariables) :: IVD
