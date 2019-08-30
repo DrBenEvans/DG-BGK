1784de6a1bfa98d56879444912994a516fa16740
Michele Mesiti <michele.mesiti@gmail.com>
2019-08-30 03:52:36 +0100

    eliminated all compilation warnings
    also, fprettified all, replaced tagged do's with dos-enddos

--- src/ifilla.f90
+++ src/ifilla.f90
@@ -1,14 +1,15 @@
-      SUBROUTINE IFILLA(MA,NA,LA,KA,val) 
-! 
-      INTEGER MA(NA,LA,KA),val 
-! 
-	  DO 1002 K=1,KA 
-      DO 1000 J=1,LA 
-      DO 1001 I=1,NA 
-      MA(I,J,K)=val 
- 1001 CONTINUE 
- 1000 CONTINUE 
- 1002 CONTINUE 
-! 
-       RETURN 
-       END 
+SUBROUTINE IFILLA(MA, NA, LA, KA, val)
+  IMPLICIT NONE
+  INTEGER NA, LA, KA, I, J, K
+  INTEGER MA(NA, LA, KA), val
+!
+  DO K = 1, KA
+    DO J = 1, LA
+      DO I = 1, NA
+        MA(I, J, K) = val
+      ENDDO
+    ENDDO
+  ENDDO
+!
+  RETURN
+END
